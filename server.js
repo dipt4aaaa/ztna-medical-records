b5cf3c34b01cb611e643764bae997938e031e9d2
dipt4aaaa <fadhilmedsos@gmail.com>
2025-06-16 19:23:37 +0700

    fix ssl

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ const port = process.env.API_PORT || 3000;
 // Database connection
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: false, // Paksa nonaktif SSL
 });
 
 // Middleware
